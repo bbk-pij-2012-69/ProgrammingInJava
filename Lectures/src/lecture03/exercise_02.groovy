69092b7c43eb7b501657c39ca57fcacd37f2e150
jsted02 <jsted02@dcs.bbk.ac.uk>
2012-10-15 19:50:19 +0100

    Added exercise 02 and delete 09 (wrong number)

--- Lectures/src/lecture03/exercise_02.groovy
+++ Lectures/src/lecture03/exercise_02.groovy
@@ -1,5 +1,66 @@
-package lecture02
+package lecture03
 def var1, var2, operation
+def result = null
+
+if(args.size() == 3)
+{
+    var1 = args[0] as double
+    operation = args[1] as String
+    var2 = args[2] as double
+}
+else if(args.size() == 1)
+{
+    if(args[0].contains("+")){
+        operation = "+"
+    }
+    else if(args[0].contains("-")){
+        operation = "-"
+    }
+    else if(args[0].contains("*")){
+        operation = "*"
+    }
+    else if(args[0].contains("/")){
+        operation = "/"
+    }
+    else{
+        println "Unhandled case!"
+        System.exit(1)
+    }
+        
+    def values = args[0].split("\\"+operation)
+    var1 = values[0] as double
+    var2 = values[1] as double
+}
+else{
+    println "Unhandled case!"
+    System.exit(1)
+}
 
 
-print args
+
+switch(operation)
+{
+    case "+":
+        result = var1 + var2
+        break
+    case "-":
+        result = var1 - var2
+        break
+    case "/":
+        if(var2 == 0)
+        {
+            println "Don't be a smart ass!"
+            break
+        }
+        result = var1 / var2
+        break
+    case "*":
+        result = var1 * var2
+        break
+    default:
+        println "Invalid operator!!"
+}
+
+if(result != null){
+    println "Result: " + result
+}
