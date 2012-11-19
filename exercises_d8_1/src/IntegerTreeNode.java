ef285432147021e1a8551b787351cecbd6bacce0
Luke Stedman <luke.stedman@gmail.com>
2012-11-19 09:23:41 +0000

    Updated IntegerTreeNode and added run.java

--- exercises_d8_1/src/IntegerTreeNode.java
+++ exercises_d8_1/src/IntegerTreeNode.java
@@ -3,7 +3,51 @@ public class IntegerTreeNode {
 	int value;
 	IntegerTreeNode left;
 	IntegerTreeNode right;
+	
+	public IntegerTreeNode(int val)
+	{
+		this.value = val;
+	}
 
-
-
+	public void add(int newNumber){
+		if(newNumber > this.value){
+			if (this.right == null){
+				this.right = new IntegerTreeNode(newNumber);
+			}
+			else{
+				this.right.add(newNumber);
+			}
+		}
+		else{
+			if (this.left == null){
+				this.left = new IntegerTreeNode(newNumber);
+			}
+			else{
+				this.left.add(newNumber);
+			}
+		}
+	}
+	
+	public boolean contains(int n){
+		if (n == this.value){
+			return true;
+		}
+		else if (n > this.value){
+			if(this.right == null){
+				return false;
+			}
+			else{
+				return this.right.contains(n);
+			}
+		}
+		else{
+			if(this.left == null){
+				return false;
+			}
+			else{
+				return this.left.contains(n); 
+			}
+		}
+	}
+	
 }
---
+++ exercises_d8_1/src/run.java
@@ -0,0 +1,15 @@
+
+
+public class run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		IntegerTreeNode itn = new IntegerTreeNode(3);
+		itn.add(4);
+		
+	}
+
+}
