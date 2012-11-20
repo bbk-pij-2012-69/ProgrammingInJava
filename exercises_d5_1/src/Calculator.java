43465725f5e13ccb7e5d9a2f4bc5ea45b20cfc2e
Luke Stedman <luke.stedman@gmail.com>
2012-11-20 15:32:43 +0000

    Added exercise 5 added.

---
+++ exercises_d5_1/src/Calculator.java
@@ -0,0 +1,29 @@
+
+public class Calculator {
+	public static void add(int x, int y)
+	{
+		System.out.println(x + y);
+	}
+	
+	public static void subtract(int x, int y)
+	{
+		System.out.println(x - y);
+	}
+	
+	public static void multiply(int x, int y)
+	{
+		System.out.println(x * y);
+	}
+	
+	public static void divide(int x, int y)
+	{
+		System.out.println(x / y);
+	}
+	
+	public static void modulus(int x, int y)
+	{
+		System.out.println(x % y);
+	}
+	
+
+}
