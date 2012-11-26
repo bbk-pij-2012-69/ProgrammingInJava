cabaefbcc77ac9e3b9efcf03cbc0587adc0f6b6c
Luke Stedman <luke.stedman@gmail.com>
2012-11-26 12:16:07 +0000

    Added d9_2.

---
+++ exercises_d9_2/src/HashUtilities.java
@@ -0,0 +1,35 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class HashUtilities {
+
+	public static int shortHash(int value)
+	{
+		int mod = 999;
+		do
+		{
+			if(value % mod == 0)
+				return mod;
+			--mod;
+		}while(mod > 1);
+		
+		return 1;
+	}
+	
+	@Test
+	public void testHash()
+	{
+		int tests = 1000;
+		while(tests-- > 0)
+		{
+			int randVal = Math.round(Math.round(Math.random() * 1024));
+			//System.out.println("Checking: " + randVal);
+			int result = HashUtilities.shortHash(randVal);
+			assertEquals("Result", true, result > 0 && result < 1000);
+		}
+	}	
+	
+}
